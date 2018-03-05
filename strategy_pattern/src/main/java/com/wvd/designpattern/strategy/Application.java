c7f4fc240f7932086457cea2f7807136aa266759
Val Wang <valwang@vipabc.com>
2018-03-05 19:04:15 +0800

    策略模式

--- strategy_pattern/src/main/java/com/wvd/designpattern/strategy/Application.java
+++ strategy_pattern/src/main/java/com/wvd/designpattern/strategy/Application.java
@@ -1,6 +1,19 @@
 package com.wvd.designpattern.strategy;
 
-
+import com.wvd.designpattern.strategy.comtext.Context;
+import com.wvd.designpattern.strategy.interfaceimp.OperationAdd;
+import com.wvd.designpattern.strategy.interfaceimp.OperationMultiply;
+import com.wvd.designpattern.strategy.interfaceimp.OperationSubstract;
 
 public class Application {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAdd());
+        System.out.println(" 7 + 8 = " + (context.executeStrategy(7, 8)));
+
+        context = new Context(new OperationSubstract());
+        System.out.println(" 7 - 8 = " + (context.executeStrategy(7, 8)));
+
+        context = new Context(new OperationMultiply());
+        System.out.println(" 7 * 8 = " + (context.executeStrategy(7, 8)));
+    }
 }
---
+++ strategy_pattern/src/main/java/com/wvd/designpattern/strategy/interfaces/Strategy.java
@@ -0,0 +1,8 @@
+package com.wvd.designpattern.strategy.interfaces;
+
+/**
+ * Created by valwang on 2018/3/5.
+ */
+public interface Strategy {
+    public int doOperation(int num1, int num2);
+}
